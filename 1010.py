92b7a089f35a92dcbe8f2f2808acd0c694b4694c
junghye01 <tweetiijh@naver.com>
2023-02-14 10:25:19 +0900

    [23.02.14] 1051

---
+++ 1010.py
@@ -0,0 +1,14 @@
+import sys
+T=int(sys.stdin.readline())
+
+def factorial(n): # mCn = m! / (m-n)!n!
+    result=1
+    for i in range(1,n+1):
+        result=result*i
+    return result
+
+
+for _ in range(T):
+    n,m=map(int,sys.stdin.readline().split())
+    bridge=factorial(m)// (factorial(m-n)*factorial(n))
+    print(bridge)
