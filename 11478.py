eaddf19e97d440aabbaa2f145460d80efa886651
junghye01 <tweetiijh@naver.com>
2024-08-12 19:38:28 +0900

    폴더정리

--- 11478.py
+++
@@ -1,8 +0,0 @@
-S=input()
-total=set()
-
-for i in range(len(S)):
-    for j in range(i,len(S)):
-        total.add(S[i:j+1])
-
-print(len(total))
--- 11660.py
+++
@@ -1,7 +0,0 @@
-import sys
-input=sys.stdin.readline
-
-N,M=map(int,input().split())
-for i in range(N):
-    for j in range(N):
-        
